e0e8ded6018cae907877aca04c800b12cfd26f0f
l1htar <l1htar@bk.ru>
2023-03-26 22:34:38 +0300

    Feat:
    add views -> temp and setting his

--- pet_shop/urls.py
+++ pet_shop/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pet_sait.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('temp', index, name='index'),
+
 ]
