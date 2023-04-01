bf357e4a6062c843aa3b195effb1ce5de88abe6c
an1 <sa-fa@mail.ru>
2023-04-01 12:02:04 +0300

    Fix: Изменил файл read_dogs.py -> теперь принимает путь на папку и возвращает готовы словарь с титлом

--- read_dogs.py
+++ read_dogs.py
@@ -1,25 +1,31 @@
 import os
 from pprint import pprint
 
-DOGS = []
 path1 = 'static/content/dogs'
 
-for name_dir in os.listdir(path1):
-    text_content = ''
-    dogs_img = ''
-    for name_file in os.listdir(f'{path1}/{name_dir}'):
-        if name_file.split('.')[1] == 'txt':
-            with open(f'{path1}/{name_dir}/{name_file}') as f:
-                text_content = f.read()
 
-        if name_file.split('.')[1] == 'jpg':
-            dogs_img = f'{path1}/{name_dir}/{name_file}'
+def return_list(path: str) -> list:
+    pets = []
+    for name_dir in os.listdir(path):
+        text_content = ''
+        dogs_img = ''
+        for name_file in os.listdir(f'{path}/{name_dir}'):
+            if name_file.split('.')[1] == 'txt':
+                with open(f'{path}/{name_dir}/{name_file}') as f:
+                    text_content = f.read()
 
-    DOGS.append({'name': name_dir, 'img': dogs_img, 'text': text_content})
+            if name_file.split('.')[1] == 'jpg':
+                dogs_img = f'{path}/{name_dir}/{name_file}'
 
-pprint(DOGS)
-DOGS = {'dogs': DOGS}
+        pets.append({'name': name_dir, 'img': dogs_img, 'text': text_content})
+    return pets
 
-    # with open(path1, 'r') as f:
-    #     res1 = f.read()
-    #     print(res1)
+
+def return_dict(path: str) -> dict:
+    title_list = path.split('/')[-1]
+    return {'title': title_list, 'pets': return_list(path)}
+
+if __name__ == '__main__':
+    pprint(return_dict(path1))
+
+
